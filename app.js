11d14065a53646c890b28bce54c900dccb53dec7
Vitor Mendes <vitor_ribeiro@id.uff.br>
2025-02-14 16:05:25 -0300

    primeira versão do amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    if (nomeAmigo == "") {
+        alert ('Por favor, insira um nome.');
+    } else {
+    amigos.push(nomeAmigo);
+    limparCampo ();
+    console.log (amigos);
+    }
+}
+
+function limparCampo () {
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
+
+let lista = document.getElementById('listaAmigos');
+lista.innerHTML = '';
+let indice = 0;
+let nomesAmigos = amigos[indice];
+//for (indice = 0, indice < amigos.length; indice++) {}
