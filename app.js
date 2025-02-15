766e53bea03454d2fcd1f96a3c85699e8a5cf68f
Vitor Mendes <vitor_ribeiro@id.uff.br>
2025-02-15 11:00:22 -0300

    projeto completo finalizado

--- app.js
+++ app.js
@@ -9,16 +9,52 @@ function adicionarAmigo() {
     amigos.push(nomeAmigo);
     limparCampo ();
     console.log (amigos);
+    adicionarLista();
+    let resultadoSorteio = document.getElementById('resultado');
+    resultadoSorteio.innerHTML = '';
     }
 }
 
 function limparCampo () {
     nomeAmigo = document.querySelector('input');
     nomeAmigo.value = '';
+    var lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
 }
 
-let lista = document.getElementById('listaAmigos');
-lista.innerHTML = '';
-let indice = 0;
-let nomesAmigos = amigos[indice];
-//for (indice = 0, indice < amigos.length; indice++) {}
+
+
+
+function adicionarLista() {
+
+    // declarar lista e limpar o campo para não ter duplicados;
+    var lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+// laço for para puxar cada nome;   
+    for (let i = 0; i < amigos.length; i++) {
+    let nomesAmigos = amigos[i];
+
+// Criar elemento;
+    var li = document.createElement('li');
+
+// Criando o nó de texto de outra forma;
+    li.textContent = nomesAmigos;
+
+// Inserir (anexar) o elemento filho (li) ao elemento pai (lista);
+    lista.appendChild(li);
+}
+}
+
+function sortearAmigo () {
+    if (amigos.length == 0) {
+    alert ('Adicione um nome.');
+    } else {
+    let pessoaEscolhida = amigos[Math.floor((Math.random() * amigos.length))];
+    let resultadoSorteio = document.getElementById('resultado');
+    resultadoSorteio.innerHTML = 'O amigo secreto sorteador é: ' + pessoaEscolhida;
+    // alert (pessoaEscolhida);
+    limparCampo();
+    amigos = [];
+}
+}
